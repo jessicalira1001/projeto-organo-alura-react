7f4032e2669c139bb2478e451424dc91b2e6a4c8
jessicalira1001 <jessicalira1001@gmail.com>
2025-05-28 21:01:17 -0300

    Adicionando componente Time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner></Banner>
-      <Formulario></Formulario>
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado }></Formulario>
+      <Time nome="Programação"> </Time>
+      <Time nome="Frontend"> </Time>
+      <Time nome="Data Science"> </Time>
+      
     </div>
   );
 }
